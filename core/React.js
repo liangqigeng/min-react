f0067f96a973dae48493a6b93ceaa79740c50085
liangqigeng <liangqigeng@mixcom.cn>
2024-01-14 23:57:13 +0800

    实现fiber框架

--- core/React.js
+++ core/React.js
@@ -20,24 +20,81 @@ function createElement(type, props, ...children) {
     };
 }
 
-function render(el, container) {
-    const dom = 
-    el.type === "TEXT_ELEMENT"
-    ? document.createTextNode("")
-    : document.createElement(el.type);
+let nextWorkOfUnit = null;
+function workLoop(dealline) {
+    let shouldYield = false;
+    while(!shouldYield && nextWorkOfUnit) {
+        nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit)
 
-    Object.keys(el.props).forEach((key) => {
+        shouldYield = dealline.timeRemaining() < 1;
+    }
+    requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function createDom(type) {
+    return type === "TEXT_ELEMENT"
+    ? document.createTextNode("")
+    : document.createElement(type);
+}
+
+function updateProps(dom, props) {
+    Object.keys(props).forEach((key) => {
         if(key !== "children") {
-            dom[key] = el.props[key];
+            dom[key] = props[key];
         }
     });
+}
 
-    const children = el.props.children;
-    children.forEach((child) => {
-        render(child, dom);
-    });
+function initChildren(fiber) {
+    const children = fiber.props.children;
+    let prevChild = null;
+    children.forEach((child, index) => {
+        const newFiber = {
+            type: child.type,
+            props:child.props,
+            child: null,
+            parent: fiber,
+            sibling: null,
+            dom: null,
+        };
 
-    container.append(dom);
+        if(index === 0) {
+            fiber.child = newFiber;
+        } else {
+            prevChild.sibling = newFiber;
+        }
+        prevChild = newFiber;
+    })
+}
+
+function performWorkOfUnit(fiber) {
+    if(!fiber.dom) {
+        const dom = (fiber.dom = createDom(fiber.type));
+        fiber.parent.dom.append(dom);
+        updateProps(dom, fiber.props);
+    }
+    initChildren(fiber);
+    // 返回下一个要执行的任务
+    if(fiber.child) {
+        return fiber.child
+    }
+
+    if(fiber.sibling) {
+        return fiber.sibling
+    }
+
+    return fiber.parent?.sibling
+}
+
+function render(el, container) {
+    nextWorkOfUnit = {
+        dom: container,
+        props: {
+            children: [el],
+        }
+    };
 }
 
 const React = {
